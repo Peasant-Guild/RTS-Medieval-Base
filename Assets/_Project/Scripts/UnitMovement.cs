9c52f179c380d248e4e44afc9625599746a4188a
TomerSivan <sivantomer10@gmail.com>
2026-03-14 18:35:27 +0200

    Add unit movement script

---
+++ Assets/_Project/Scripts/UnitMovement.cs
@@ -0,0 +1,30 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class UnitMovement : MonoBehaviour
+{
+    [SerializeField] private LayerMask ground;
+    
+    private Camera _cam;
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _cam = Camera.main;
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Mouse.current != null && Mouse.current.rightButton.isPressed)
+        {
+            Ray ray = _cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, ground))
+                {
+                _agent.SetDestination(hit.point);
+                }
+        }
+    }
+}

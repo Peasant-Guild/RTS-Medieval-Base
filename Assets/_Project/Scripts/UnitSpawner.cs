51ad5deb44c3c9c3505da4a658093d0dd95ef739
UdiReuveni <reuveniudi1@gmail.com>
2026-03-19 21:43:32 +0200

    Add queue-merging of adjacent creations of the same type

--- Assets/_Project/Scripts/UnitSpawner.cs
+++ Assets/_Project/Scripts/UnitSpawner.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 using System.Collections.Generic;
+using System.Linq;
 
 public class UnitSpawner : MonoBehaviour
 {
@@ -22,15 +23,12 @@ public class UnitSpawner : MonoBehaviour
         }
     }
     
-    // [SerializeField] private float _spawnTime = 2f;
-    // [SerializeField] private bool _spawnIndefinitely;
-    // [SerializeField] private int _currentlySpawning = 0;
 
-    [SerializeField] private float _spawnCountdownTimer;
+    [SerializeField] private float _spawnCountdownTimer; //is a serialized field for debugging
 
     //offsets are relative to the *center* of the building
     [SerializeField] private Vector3 _entranceSpawnOffset;
-    [SerializeField] private Vector3 _outsideOffset; //walk out position
+    [SerializeField] private Vector3 _outsideOffset; //walk out position (creation animation)
     
     
     private GameObject _currentUnit = null;
@@ -41,9 +39,8 @@ public class UnitSpawner : MonoBehaviour
     private void Start()
     {
         _spawnCountdownTimer = 0;
-        // CallSpawn(_unitType, 4, 3f);
-        // CallSpawn(_unitType, 2, 10f);
     }
+    
     private void Update()
     {
         if (_spawnCountdownTimer > 0f)
@@ -57,10 +54,6 @@ public class UnitSpawner : MonoBehaviour
                 SpawnUnit(_currentUnit);
             }
             
-            // if (!_spawnIndefinitely)
-            // {
-            //     _currentlySpawning -= 1;
-            // }
             if (_unitsToSpawn.Count <= 0)
             {
                 _currentUnit = null;
@@ -68,7 +61,6 @@ public class UnitSpawner : MonoBehaviour
             }
 
             LoadNextUnit();
-            
             //whether or not we are currently working is determined by the _currentUnit (if its null, we're idle)
         }
     }
@@ -88,45 +80,29 @@ public class UnitSpawner : MonoBehaviour
         }
     }
     
-    // private void OnMouseDown() //debug purposes only
-    // {
-    //     _spawnCountdownTimer = -1f;
-    //     CallSpawn(_unitType, 4, 3f);
-    //     CallSpawn(_unitType, 2, 10f);
-    // }
     private void CallSpawn(GameObject unit, int amount = 1, float spawnTime = 2f)
     {
         if (unit == null || amount <= 0 || spawnTime <= 0f)
         {
             return;
         }
-        
-        _unitsToSpawn.Enqueue(new spawnInfo(unit, amount, spawnTime));
-        
-        
-        // _currentlySpawning += amount;
-    }
 
-    // public void StartIndefiniteSpawning()
-    // {
-    //     _spawnIndefinitely = true;
-    //     _currentlySpawning = 0;
-    // }
-    // public void StopIndefiniteSpawning()
-    // {
-    //     _spawnIndefinitely = false;
-    //     _currentlySpawning = 0;
-    // }
+        if (_unitsToSpawn.Count > 0 && _unitsToSpawn.Last().gameObject == unit)
+        {
+            _unitsToSpawn.Last().amount+= amount;
+        }
+        else
+        {
+            _unitsToSpawn.Enqueue(new spawnInfo(unit, amount, spawnTime));
+        }
+    }
+    
     private void SpawnUnit(GameObject unitToSpawn)
     {
         GameObject unit_obj = Instantiate(unitToSpawn, transform.position + _entranceSpawnOffset, transform.rotation);
         UnitMovement walker = unit_obj.GetComponent<UnitMovement>();
         walker.MoveTo(transform.position + _outsideOffset);
         
-        //needs waypoints!
-        // if (_destination != Vector3.zero)
-        // {
-        //     walker.MoveTo(_destination);
-        // }
+        //TODO: needs waypoints in order to send to a final destination
     }
 }
